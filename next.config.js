a58861bf2c236ddb1bb9cfd00fbf27c410303ee8
philogicae <38438271+philogicae@users.noreply.github.com>
2024-01-17 00:07:21 +0200

    Setup

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  env: {
+    appVersion: process.env.npm_package_version,
+  },
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+    return config;
+  },
+};
+
+module.exports = nextConfig;
