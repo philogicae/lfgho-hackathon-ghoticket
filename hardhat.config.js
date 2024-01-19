7049bea39796b9505dccadd4f2ae918a1c5a08d2
philogicae <38438271+philogicae@users.noreply.github.com>
2024-01-19 02:56:10 +0200

    Add hardhat + Upgrade NextJS 14.1

---
+++ hardhat.config.js
@@ -0,0 +1,22 @@
+require('@nomicfoundation/hardhat-toolbox')
+require('dotenv').config()
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    version: '0.8.23',
+    settings: {
+      evmVersion: 'shanghai',
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    sepolia: {
+      url: 'https://sepolia.infura.io/v3/' + process.env.NEXT_PUBLIC_INFURA_ID,
+      accounts: [process.env.TEST_PRIVATE_KEY],
+    },
+  },
+}
