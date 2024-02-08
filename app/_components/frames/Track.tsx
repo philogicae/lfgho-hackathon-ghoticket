4d57f4e84397c5feea0959b2cbb4143ff5271be8
philogicae <38438271+philogicae@users.noreply.github.com>
2024-02-08 02:40:38 +0200

    Track 1

--- app/_components/frames/Track.tsx
+++ app/_components/frames/Track.tsx
@@ -1,33 +1,86 @@
 'use client'
-//import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { cn } from '@utils/tw'
-//import { useAccount } from 'wagmi'
-//import load from '@contracts/loader'
-//import { useTransact } from '@components/hooks/Transact'
+import { useAccount } from 'wagmi'
+import load, { ContractData } from '@contracts/loader'
 import Title from '@components/elements/Title'
 import { FaMagnifyingGlass, FaWandMagicSparkles } from 'react-icons/fa6'
 //import { useParams } from 'react-router-dom'
+import { useChains } from 'connectkit'
+import { useCall } from '@components/hooks/Caller'
 
 export default function Track() {
   //const { addr } = useParams()
-  //const { isConnected, address, chainId } = useAccount()
-  //const contract = load('QRFlow', chainId)
-  //const [isLoading, setIsLoading] = useState(false)
+  const { isConnected, address } = useAccount()
+  const chains = useChains()
+  const contracts = chains
+    .map((c) => ({ chainId: c.id, contract: load('QRFlow', c.id) }))
+    .filter((x) => !!x.contract) as {
+    chainId: number
+    contract: ContractData
+  }[]
+  const [contractData, setContractData] = useState<number[]>([])
+  const { result: resultData } = useCall({
+    calls: contracts.map((x) => ({
+      ...x,
+      functionName: 'getAccountNonce',
+      args: [address!],
+    })),
+    initData: new Array(contracts.length).fill(BigInt(0)),
+    active: isConnected && !contractData.length,
+  })
+  useEffect(() => {
+    if (resultData) setContractData(resultData.getAccountNonce.map(Number))
+  }, [resultData])
+  const [isLoading, setIsLoading] = useState(false)
+  useEffect(() => {
+    isLoading && setTimeout(() => setIsLoading(false), 2000)
+  }, [isLoading])
   return (
     <>
       <Title
         label="Tracking"
         logo={<FaMagnifyingGlass className="transform -scale-x-100" />}
+        loading={isLoading}
+        ready={true}
+        onClick={() => setIsLoading(true)}
       />
       <div
         className={cn(
-          'flex flex-col h-full w-full min-w-[320px] max-w-[700px] border border-cyan-400 mt-2 items-center justify-start overflow-hidden rounded-xl'
+          'flex flex-col h-full min-w-[320px] max-w-[700px] border border-cyan-400 mt-2 items-center justify-start overflow-hidden rounded-xl bg-blue-800 bg-opacity-10'
         )}
       >
+        <div className="border-b-1 text-cyan-300 border-cyan-300 flex w-full h-12 items-center justify-between font-mono tracking-tighter text-base">
+          <div />
+          WIP
+          {/* <span className={cn('animate-pulse text-orange-400 font-bold')}>
+            1. Sign Tickets
+          </span>
+          <span className={cn('animate-pulse text-orange-400 font-bold')}>
+            2. Sign Approval
+          </span>
+          <span className={cn('animate-pulse text-orange-400 font-bold')}>
+            3. Deposit $GHO
+          </span> */}
+          <div />
+        </div>
         <div className="flex flex-col items-center justify-center w-full h-full mb-10">
           <FaWandMagicSparkles className="mb-4 text-6xl" />
-          <span className="text-xl font-bold w-48 text-center">
-            Not Ready Yet
+          <span className="text-base font-bold w-48 text-center">
+            {!!contractData.length
+              ? JSON.stringify(
+                  contractData.map((x, i) => ({
+                    chain: contracts[i].chainId,
+                    addr:
+                      contracts[i].contract.address.slice(0, 4) +
+                      '...' +
+                      contracts[i].contract.address.slice(-4),
+                    nonce: x,
+                  })),
+                  null,
+                  2
+                )
+              : 'Not Ready Yet'}
           </span>
         </div>
       </div>
--- app/_components/hooks/Caller.tsx
+++ app/_components/hooks/Caller.tsx
@@ -16,6 +16,12 @@ type CallsProps = {
 }
 
 const useCall = ({ calls, initData, active = false }: CallsProps) => {
+  const methods: { [method: string]: number } = {}
+  calls
+    .map((x) => x.functionName)
+    .forEach((name) => {
+      methods[name] = (methods[name] || 0) + 1
+    })
   const reads = useReadContracts({
     contracts: calls.map((x) => ({
       chainId: x.chainId,
@@ -25,23 +31,28 @@ const useCall = ({ calls, initData, active = false }: CallsProps) => {
     })),
     query: {
       enabled: active,
-      select: (data) =>
-        Object.assign(
-          {},
-          ...(data && data.length
-            ? data.map((x, i) => {
-                const field = initData[i]
-                return x.status === 'success'
-                  ? (x.result as typeof field)
-                  : initData[i]
-              })
-            : initData
-          ).map((x, i) => ({ [calls[i].functionName]: x }))
-        ),
+      select: (data) => {
+        const result: { [method: string]: any } = {}
+        ;(data && data.length
+          ? data.map((x, i) => {
+              const field = initData[i]
+              return x.status === 'success'
+                ? (x.result as typeof field)
+                : initData[i]
+            })
+          : initData
+        ).forEach((x: any, i: number) => {
+          result[calls[i].functionName] =
+            methods[calls[i].functionName] > 1
+              ? [...(result[calls[i].functionName] || []), x]
+              : x
+        })
+        return result
+      },
     },
   })
   return {
-    result: reads.data,
+    result: reads.data!,
     fetch: reads.refetch,
     isLoading: reads.isLoading,
     isSuccess: reads.isSuccess,
